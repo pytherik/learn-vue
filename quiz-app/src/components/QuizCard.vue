11370aef6807b8fd5dad453963b309fbd3a421d0
Erik Berndt <erikberndt@gmx.net>
2023-10-09 14:24:53 +0200

    animated quiz-cards

--- quiz-app/src/components/QuizCard.vue
+++ quiz-app/src/components/QuizCard.vue
@@ -1,23 +1,22 @@
 <script setup>
-
-import {defineProps, ref} from "vue";
-import {useRouter} from "vue-router";
-const props = defineProps(['quiz']);
+import { defineProps, ref } from "vue";
+import { useRouter } from "vue-router";
+const props = defineProps(["quiz"]);
 const quiz = ref(props.quiz);
 const router = useRouter();
 const navigateToQuiz = () => {
-  router.push(`/questions/${quiz.value.id}`)
-}
+  router.push(`/questions/${quiz.value.id}`);
+};
 </script>
 
 <template>
-        <div class="card" @click="navigateToQuiz()">
-          <img :src="quiz.img" :alt="quiz.name" />
-          <div class="card-text">
-            <h2>{{ quiz.name }}</h2>
-            <p>{{ quiz.questions.length }} questions</p>
-          </div>
-        </div>
+  <div class="card" @click="navigateToQuiz()">
+    <img :src="quiz.img" :alt="quiz.name" />
+    <div class="card-text">
+      <h2>{{ quiz.name }}</h2>
+      <p>{{ quiz.questions.length }} questions</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -30,6 +29,7 @@ const navigateToQuiz = () => {
 .card {
   width: 310px;
   overflow: hidden;
+  border: none;
   border-radius: 2%;
   box-shadow: 1px 1px 10px #00000033;
   margin-right: 10px;
@@ -42,5 +42,4 @@ const navigateToQuiz = () => {
 .card .card-text h2 {
   font-weight: bold;
 }
-
 </style>
--- quiz-app/src/views/QuizesView.vue
+++ quiz-app/src/views/QuizesView.vue
@@ -2,13 +2,31 @@
 import q from "../data/data.json";
 import { ref, watch } from "vue";
 import QuizCard from "@/components/QuizCard.vue";
-
+import gsap from "gsap";
 let quizes = ref(q);
 const search = ref("");
 
 watch(search, () => {
-    quizes.value = q.filter((quiz) => quiz.name.toLowerCase().includes(search.value.toLowerCase()));
-})
+  quizes.value = q.filter((quiz) =>
+    quiz.name.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
+
+const beforeEnter = (el) => {
+  el.style.opacity = 0;
+  el.style.transform = "translateY(-100px)";
+};
+const enter = (el) => {
+  gsap.to(el, {
+    y: 0,
+    opacity: 1,
+    duration: 0.3,
+    delay: 0.3 * el.dataset.idx,
+  });
+};
+const afterEnter = () => {
+  console.log("after enter");
+};
 </script>
 
 <template>
@@ -18,13 +36,24 @@ watch(search, () => {
       <input v-model.trim="search" type="text" placeholder="Suchen..." />
     </header>
     <div class="options-container">
-      <QuizCard v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+      <TransitionGroup
+        appear
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @after-enter="afterEnter"
+      >
+        <QuizCard
+          v-for="(quiz, idx) in quizes"
+          :key="quiz.id"
+          :quiz="quiz"
+          :data-idx="idx"
+        />
+      </TransitionGroup>
     </div>
   </div>
 </template>
 
 <style scoped>
-
 header {
   margin: 10px 0;
   display: flex;
@@ -50,5 +79,4 @@ header input {
   flex-wrap: wrap;
   margin-top: 40px;
 }
-
 </style>
