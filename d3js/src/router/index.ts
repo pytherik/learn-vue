f72c3aa0523fb99ff94dc7fe6c1f61e59f003c9a
pytherik <erikberndt@gmx.net>
2023-10-17 15:51:43 +0200

    d3.js and chart.js

---
+++ d3js/src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ChartView from '@/views/ChartView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/about', name: 'about', component: AboutView },
+    { path: '/chart', name: 'chart', component: ChartView }
+  ]
+})
+
+export default router
